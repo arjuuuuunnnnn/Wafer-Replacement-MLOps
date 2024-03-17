f22fc010f873d02ebb040180657f8308de504e74
Hemanth <hemanth@kali.hemanth>
2024-03-17 10:37:41 +0530

    Outline done hemanth

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To predict whether to replace a wafer or not',
+    author='arjuuuuunnnnn',
+    license='MIT',
+)
