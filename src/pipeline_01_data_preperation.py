17534489043dbb3edf91b767638e058d5af9c304
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-03-18 18:50:28 +0530

    read the config file's params

--- src/pipeline_01_data_preperation.py
+++ src/pipeline_01_data_preperation.py
@@ -4,6 +4,14 @@ import yaml
 import logging
 
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml_file.safe_load(yaml_file)
+    return config
+
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print(config)
 
 
 
@@ -11,11 +19,13 @@ import logging
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="default")
+    default_config_path = os.path.join("config", "params.yaml") # this is done because if I give config/params.yaml directly it might not correctly work in windows system
+    args.add_argument("--config", default=default_config_path)
     args.add_argument("--datasource", default=None)
     # adding command line arguements
 
     parsed_args = args.parse_args()
-    print(parsed_args) # to know the values passed by the user in the command line as arguments
+    # print(parsed_args) # to know the values passed by the user in the command line as arguments
 
+    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
     
